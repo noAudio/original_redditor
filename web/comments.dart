7a266ee4ea0d44697e651a403f4e802abca289f7
Samuel Odongo <samieodd@gmail.com>
2021-12-15 23:14:28 +0300

    added list of the most repeated unoriginal reddit comments
    Small sample size of common comments posted on literally every thread ever

---
+++ web/comments.dart
@@ -0,0 +1,11 @@
+List<String> comments = [
+  'Nice',
+  'I choose this guy\'s dead wife too',
+  'Username checks out',
+  'This',
+  'Your post deserves more upvotes',
+  '*Sigh* unzips',
+  'Step one: be attractive. Step two: don\'t be unattractive',
+  'r/whoosh',
+  'F',
+];
